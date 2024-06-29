0b09f6cd41df572ca6d4cbd4b6e1602cc41df6e8
Rango Ack <rangoack@proton.me>
2024-06-29 14:39:34 +0800

    update the style of the connect page

--- src/pages/Connect.tsx
+++ src/pages/Connect.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Button from '../components/Button';
 import ModalListItem from '../components/ModalListItem';
@@ -26,56 +26,60 @@ const Connect = ({
 		// the user should be prompted with `/` route
 		throw new Error('derivedAddresses does not exist');
 	}
+	useEffect(() => {
+		const root = document.getElementById('root');
+		root?.classList.remove('root');
+		root?.classList.add('h-full');
+	}, [])
 
 	return (
-		<div className="h-full flex flex-col">
-			<div className="fy p-4 bg-skin-middleground">
-				<p className="text-lg text-center">{i18n.vitePassportIsLinking}</p>
-				<div className="mt-2 px-5 py-3 bg-skin-base rounded-full">
-					<p className="leading-3 text-lg break-words">{hostname}</p>
+			<div className="h-full flex flex-col">
+				<section className="radial-green2blue z-10 rounded-b-[24px]">
+					<div className="py-6 text-lg text-center text-white">{i18n.vitePassportIsLinking}</div>
+					<div className="mb-3 mx-5 px-4 py-1 bg-white rounded-full text-center">
+						<p className="text-lg break-words text-skin-highlight">{hostname}</p>
+					</div>
+				</section>
+				<div className="flex-1 overflow-scroll">
+					{derivedAddresses.map((address, i) => {
+						const active = i === lastActiveAccountIndex;
+						return (
+							<ModalListItem
+								radio
+								key={address}
+								active={active}
+								className="flex-1"
+								label={contacts[address]}
+								sublabel={shortenAddress(address)}
+								onClick={() => lastActiveAccountIndexSet(i)}
+							/>
+						);
+					})}
+				</div>
+				<div className="fx p-4 gap-4">
+					<Button theme="lowlight" label={i18n.cancel} onClick={() => window.close()} />
+					<Button
+						theme="highlight"
+						label={i18n.confirm}
+						onClick={async () => {
+							const activeAddress = derivedAddresses[lastActiveAccountIndex];
+							triggerInjectedScriptEvent({ type: 'connectWallet', payload: { domain: hostname } });
+							triggerInjectedScriptEvent({
+								type: 'accountChange',
+								payload: { activeAddress },
+							});
+							if (!connectedDomains[activeAddress]) {
+								connectedDomains[activeAddress] = {};
+							}
+							if (!connectedDomains[activeAddress][hostname]) {
+								connectedDomains[activeAddress][hostname] = {};
+							}
+							await setValue({ connectedDomains, activeAccountIndex: lastActiveAccountIndex });
+							window.close();
+						}}
+					/>
 				</div>
 			</div>
-			<div className="flex-1 overflow-scroll">
-				{derivedAddresses.map((address, i) => {
-					const active = i === lastActiveAccountIndex;
-					return (
-						<ModalListItem
-							radio
-							base
-							key={address}
-							active={active}
-							className="flex-1"
-							label={contacts[address]}
-							sublabel={shortenAddress(address)}
-							onClick={() => lastActiveAccountIndexSet(i)}
-						/>
-					);
-				})}
-			</div>
-			<div className="fx p-4 gap-4">
-				<Button theme="foreground" label={i18n.cancel} onClick={() => window.close()} />
-				<Button
-					theme="highlight"
-					label={i18n.confirm}
-					onClick={async () => {
-						const activeAddress = derivedAddresses[lastActiveAccountIndex];
-						triggerInjectedScriptEvent({ type: 'connectWallet', payload: { domain: hostname } });
-						triggerInjectedScriptEvent({
-							type: 'accountChange',
-							payload: { activeAddress },
-						});
-						if (!connectedDomains[activeAddress]) {
-							connectedDomains[activeAddress] = {};
-						}
-						if (!connectedDomains[activeAddress][hostname]) {
-							connectedDomains[activeAddress][hostname] = {};
-						}
-						await setValue({ connectedDomains, activeAccountIndex: lastActiveAccountIndex });
-						window.close();
-					}}
-				/>
-			</div>
-		</div>
 	);
 };
 
